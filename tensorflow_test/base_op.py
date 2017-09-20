4e771a54368236b5889e905e88d65ebbd75ad11c
midorra <midorra@163.com>
2017-09-20 16:28:56 +0800

    update base_op code

--- tensorflow_test/base_op.py
+++ tensorflow_test/base_op.py
@@ -2,9 +2,33 @@
 
 import tensorflow as tf
 
-a = tf.constant(123)
-b = tf.constant(456)
+## 定义常量
+A = tf.constant(123)
+B = tf.constant(456)
+
+## 定义变量
+a = tf.placeholder(tf.int32)
+b = tf.placeholder(tf.int32)
+
+## 定义加法操作
+add = tf.add(a, b)
+## 定义乘法操作
+mul = tf.multiply(a, b)
 
 with tf.Session() as sess:
-    print (sess.run(a + b))
+    print sess.run(A + B)
+    print sess.run(add, feed_dict={a:11, b:22})
+    print sess.run(mul, feed_dict={a:11, b:22})
+
+## 定义常量矩阵
+matrix_a = tf.constant([[5.,5.]])
+matrix_b = tf.constant([[7.],[9.]])
+
+## 定义矩阵乘法
+product = tf.matmul(matrix_a, matrix_b)
+
+with tf.Session() as sess:
+    print sess.run(matrix_a)
+    print sess.run(matrix_b)
+    print sess.run(product)
 
