bf58b8922c2990728794ba9df11004a87599bcaf
midorra <midorra@163.com>
2017-10-06 11:21:24 +0800

    update linear_regression

--- tensorflow_test/linear_regression.py
+++ tensorflow_test/linear_regression.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 rng = numpy.random
 
 # Parameters
-learning_rate = 0.01
-training_epochs = 1000
-display_step = 50
+LEARNING_RATE = 0.01
+TRAINING_EPOCHS = 1000
+DISPLAY_STEP = 50
 
 # Training Data
 train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
@@ -31,7 +31,7 @@ pred = tf.add(tf.multiply(X, W), b)
 # Mean squared error
 cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
 # Gradient descent
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cost)
 
 # Initializing the variables
 init = tf.initialize_all_variables()
@@ -40,13 +40,16 @@ init = tf.initialize_all_variables()
 with tf.Session() as sess:
     sess.run(init)
 
+    print 'DEBUG =====>'
+
     # Fit all training data
-    for epoch in range(training_epochs):
+    for epoch in range(TRAINING_EPOCHS):
+
         for (x, y) in zip(train_X, train_Y):
             sess.run(optimizer, feed_dict={X: x, Y: y})
 
         # Display logs per epoch step
-        if (epoch+1) % display_step == 0:
+        if (epoch+1) % DISPLAY_STEP == 0:
             c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
             print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
                 "W=", sess.run(W), "b=", sess.run(b))
@@ -70,8 +73,7 @@ with tf.Session() as sess:
         tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * test_X.shape[0]),
         feed_dict={X: test_X, Y: test_Y})  # same function as cost above
     print("Testing cost=", testing_cost)
-    print("Absolute mean square loss difference:", abs(
-        training_cost - testing_cost))
+    print("Absolute mean square loss difference:", abs(training_cost - testing_cost))
 
     plt.plot(test_X, test_Y, 'bo', label='Testing data')
     plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
